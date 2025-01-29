2bef9b1522d533e186c0746a435624f9d1f303cf
frankschweitzer <schwfj20@wfu.edu>
2025-01-29 16:06:48 -0500

    Initial Commit

---
+++ connectED/connectEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectEDApp.swift
+//  connectED
+//
+//  Created by Frank Schweitzer on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct connectEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ connectEDTests/connectEDTests.swift
@@ -0,0 +1,17 @@
+//
+//  connectEDTests.swift
+//  connectEDTests
+//
+//  Created by Frank Schweitzer on 1/29/25.
+//
+
+import Testing
+@testable import connectED
+
+struct connectEDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
